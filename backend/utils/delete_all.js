5612a81ee87d88c0884a6a3796a272a544586558
unknown <staracenapuga@gmail.com>
2023-04-13 19:07:04 -0600

    Finished delete_all.js

--- backend/utils/delete_all.js
+++ backend/utils/delete_all.js
@@ -1,3 +1,13 @@
+/**
+ * Universidad del Valle de Guatemala
+ * (CC3089) Bases de Datos 2
+ * Proyecto 2 - Pocket HBase
+ * Miembros del grupo 7:
+ *   - Pedro Pablo Arriola Jiménez (20188)
+ *   - Marco Pablo Orozco Saravia (20857)
+ *   - Santiago Taracena Puga (20017)
+ */
+
 const fs = require('fs')
 const { list } = require('./list')
 
@@ -23,6 +33,15 @@ const delete_all = (table, rowKey, args = []) => {
   })
 
   const json = JSON.parse(data)
+
+  if (!json.enabled) {
+    return {
+      method: 'put',
+      status: 'error',
+      type: 'individual',
+      data: `Table "${table}" is disabled`,
+    }
+  }
 
   json.updated = new Date().getTime()
 
